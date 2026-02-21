dc0969b6d4311ef6a29604b4b5ff1a3823933e13
Kaiserufer <eva.ramelow@gsi-hamburg.de>
2026-02-22 00:39:55 +0100

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://oqjcbxnbododdqlbdekt.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xamNieG5ib2RvZGRxbGJkZWt0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0MTg3NzAsImV4cCI6MjA4Njk5NDc3MH0.2Ig0I_Wd26LAX7FAVTUz9SdJFaLeAOh394pT3FT6i_w'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
+)
